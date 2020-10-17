159aea423a038b0417753e52a59defe0c4df8411
Framework2232 <70790825+framework2232@users.noreply.github.com>
2020-10-18 00:29:44 +1100

    head tags . html
    panic!! the form searched the URL Argh!! so trying atribute change in <form> tag.

--- headtags/headtags.js
+++ headtags/headtags.js
@@ -43,8 +43,7 @@ form.addEventListener("submit", (e) => {
     meta.setAttribute('style', 'white-space: pre;');
     meta.textContent = '<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n\t<title>'+siteName.value+'</title>\n\t<meta name="robots" content="index, follow">\n\t<meta name="description" content="'+siteDescription.value+'">\n\t<meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">\n\t<meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">\n\t<link rel="canonical" href="'+siteURL.value+'">\n\t<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\n\t<link rel="stylesheet" href="css/style.css" type="text/css"></link>\n\t<link rel="manifest" href="manifest.json">\n\t<link rel="icon" type="image/x-icon" href="favicon.ico">\n\t<link rel="icon" type="image/png" href="favicon.png">\n</head>\n<body>\n\n\n</body>\n</html>'
     
-    
-   
+     
   
 
 })
