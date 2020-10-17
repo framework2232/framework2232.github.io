40662c5c8776770593bc229d61adc319f0cbcca2
Framework2232 <70790825+framework2232@users.noreply.github.com>
2020-10-18 01:35:03 +1100

    headd tags fixed
    OK :) smiles now. form removed. btns working good.

--- headtags/headtags.js
+++ headtags/headtags.js
@@ -4,7 +4,7 @@ const resultURL = document.getElementById("resultURL");
 const resultDescription = document.getElementById("resultDescription");
 const meta = document.getElementById("metaDataBoilerPlate");
 
-// form.addEventListener("submit", (e) => { 
+
 function build() {    
 
     if (siteName.value === "" || siteName.value == null) {        
@@ -39,32 +39,26 @@ function build() {
     if (siteDescription.value.length > 150){
     resultDescription.innerText = siteDescription.value + "...";
     }    
-
     
     meta.setAttribute('style', 'white-space: pre;');
-    meta.textContent = '<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n\t<title>'+siteName.value+'</title>\n\t<meta name="robots" content="index, follow">\n\t<meta name="description" content="'+siteDescription.value+'">\n\t<meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">\n\t<meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">\n\t<link rel="canonical" href="'+siteURL.value+'">\n\t<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\n\t<link rel="stylesheet" href="css/style.css" type="text/css"></link>\n\t<link rel="manifest" href="manifest.json">\n\t<link rel="icon" type="image/x-icon" href="favicon.ico">\n\t<link rel="icon" type="image/png" href="favicon.png">\n</head>\n<body>\n\n\n</body>\n</html>'
-    
-}
-  
+    meta.textContent = '<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n\t<title>'+siteName.value+'</title>\n\t<meta name="robots" content="index, follow">\n\t<meta name="description" content="'+siteDescription.value+'">\n\t<meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">\n\t<meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">\n\t<link rel="canonical" href="'+siteURL.value+'">\n\t<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\n\t<link rel="stylesheet" href="css/style.css" type="text/css"></link>\n\t<link rel="manifest" href="manifest.json">\n\t<link rel="icon" type="image/x-icon" href="favicon.ico">\n\t<link rel="icon" type="image/png" href="favicon.png">\n</head>\n<body>\n\n\n</body>\n</html>'    
+} 
 
-// })
 
-// form.addEventListener("reset", (e) => {  
 function clearAll() {    
     siteName.value === "";
-    document.getElementById("siteName").style.borderColor="orange";
+    document.getElementById("siteName").style.borderColor="grey";
     resultName.innerText = "Website Page Title";
+    document.getElementById("siteName").value="";
 
     siteURL.value === "";
-    document.getElementById("siteURL").style.borderColor="orange";
+    document.getElementById("siteURL").style.borderColor="grey";
     resultURL.innerText = "www.yourURLhere.com";
+    document.getElementById("siteURL").value="";
 
     siteDescription.value === "";
-    document.getElementById("siteDescription").style.borderColor="orange";
+    document.getElementById("siteDescription").style.borderColor="grey";
     resultDescription.innerText = "Website Page Description goes here. Recommended between 50 - 150 characters long";
+    document.getElementById("siteDescription").value="";
     meta.textContent = "";
 }
-// })
-
-
-
