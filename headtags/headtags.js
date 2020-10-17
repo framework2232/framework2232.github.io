724f5eff6a9a5995f485f0f896caf1a0ccfb1ba5
Framework2232 <70790825+framework2232@users.noreply.github.com>
2020-10-17 23:08:47 +1100

    release head tags page
    included Head tags and Meta Data page onto main site.

---
+++ headtags/headtags.js
@@ -0,0 +1,60 @@
+
+const resultName = document.getElementById("resultName");
+const resultURL = document.getElementById("resultURL");
+const resultDescription = document.getElementById("resultDescription");
+const meta = document.getElementById("metaDataBoilerPlate");
+
+form.addEventListener("submit", (e) => {     
+
+    if (siteName.value === "" || siteName.value == null) {        
+        document.getElementById("siteName").style.borderColor="red";
+        resultName.innerText = "Website Page Title";
+    }    
+    if (siteName.value != "") {
+        document.getElementById("siteName").style.borderColor="green";
+        resultName.innerText = siteName.value;
+    }
+
+
+    if (siteURL.value === "" || siteURL.value == null) {
+        document.getElementById("siteURL").style.borderColor="red";
+        resultURL.innerText = "www.yourURLhere.com";
+    }       
+    if (siteURL.value != "") {
+        document.getElementById("siteURL").style.borderColor="green";
+        resultURL.innerText = siteURL.value;
+    }
+
+
+    if (siteDescription.value === "" || siteDescription.value == null) {
+        document.getElementById("siteDescription").style.borderColor="red";
+        resultDescription.innerText = "Website Page Description goes here. Recommended between 50 - 150 characters long";
+    }       
+    if (siteDescription.value != "") {
+        document.getElementById("siteDescription").style.borderColor="green";
+        resultDescription.innerText = siteDescription.value;
+    }         
+
+    if (siteDescription.value.length > 150){
+    resultDescription.innerText = siteDescription.value + "...";
+    }    
+
+    
+    meta.setAttribute('style', 'white-space: pre;');
+    meta.textContent = '<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n\t<title>'+siteName.value+'</title>\n\t<meta name="robots" content="index, follow">\n\t<meta name="description" content="'+siteDescription.value+'">\n\t<meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">\n\t<meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">\n\t<link rel="canonical" href="'+siteURL.value+'">\n\t<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\n\t<link rel="stylesheet" href="css/style.css" type="text/css"></link>\n\t<link rel="manifest" href="manifest.json">\n\t<link rel="icon" type="image/x-icon" href="favicon.ico">\n\t<link rel="icon" type="image/png" href="favicon.png">\n</head>\n<body>\n\n\n</body>\n</html>'
+    
+    
+   
+  
+
+})
+
+form.addEventListener("reset", (e) => {      
+    siteName.value === "";
+    siteURL.value === "";
+    siteDescription.value === "";
+    meta.textContent = "";
+})
+
+
+
