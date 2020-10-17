5780a1c74f2cef23228af4bb54fc899163ae8b73
Framework2232 <70790825+framework2232@users.noreply.github.com>
2020-10-18 01:27:03 +1100

    removed form tag
    panic not over yet but working. Removed form tag.
    CLR button not 100% yet.

--- headtags/headtags.js
+++ headtags/headtags.js
@@ -4,7 +4,8 @@ const resultURL = document.getElementById("resultURL");
 const resultDescription = document.getElementById("resultDescription");
 const meta = document.getElementById("metaDataBoilerPlate");
 
-form.addEventListener("submit", (e) => {     
+// form.addEventListener("submit", (e) => { 
+function build() {    
 
     if (siteName.value === "" || siteName.value == null) {        
         document.getElementById("siteName").style.borderColor="red";
@@ -43,17 +44,27 @@ form.addEventListener("submit", (e) => {
     meta.setAttribute('style', 'white-space: pre;');
     meta.textContent = '<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n\t<title>'+siteName.value+'</title>\n\t<meta name="robots" content="index, follow">\n\t<meta name="description" content="'+siteDescription.value+'">\n\t<meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">\n\t<meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">\n\t<link rel="canonical" href="'+siteURL.value+'">\n\t<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\n\t<link rel="stylesheet" href="css/style.css" type="text/css"></link>\n\t<link rel="manifest" href="manifest.json">\n\t<link rel="icon" type="image/x-icon" href="favicon.ico">\n\t<link rel="icon" type="image/png" href="favicon.png">\n</head>\n<body>\n\n\n</body>\n</html>'
     
-     
+}
   
 
-})
+// })
 
-form.addEventListener("reset", (e) => {      
+// form.addEventListener("reset", (e) => {  
+function clearAll() {    
     siteName.value === "";
+    document.getElementById("siteName").style.borderColor="orange";
+    resultName.innerText = "Website Page Title";
+
     siteURL.value === "";
+    document.getElementById("siteURL").style.borderColor="orange";
+    resultURL.innerText = "www.yourURLhere.com";
+
     siteDescription.value === "";
+    document.getElementById("siteDescription").style.borderColor="orange";
+    resultDescription.innerText = "Website Page Description goes here. Recommended between 50 - 150 characters long";
     meta.textContent = "";
-})
+}
+// })
 
 
 
